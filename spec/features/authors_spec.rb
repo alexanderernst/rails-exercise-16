703da95447ce9dbf10ca2a342d5f672dc7c421d3
alexanderernst <leverkusener77@googlemail.com>
2016-10-20 14:13:18 +0200

    added new author template closes #1

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -4,6 +4,6 @@ describe "New Author Page", :type => :feature do
   
   it "should render'" do
     visit new_author_path
-    expect(page).to render_template("author/new")
+    expect(page).to have_text("New Author")
   end
 end
