05e2447b715b5dc146e7e925e24f0fd4be467155
alexanderernst <leverkusener77@googlemail.com>
2016-10-21 09:32:06 +0200

    failing test for #2

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -6,4 +6,11 @@ describe "New Author Page", :type => :feature do
     visit new_author_path
     expect(page).to have_text("New Author")
   end
+
+  it "should have input for firstname, lastname, homepage" do
+    visit new_author_path
+    expect(page).to have_field("first name")
+    expect(page).to have_field("last name")
+    expect(page).to have_field("homepage")
+  end
 end
