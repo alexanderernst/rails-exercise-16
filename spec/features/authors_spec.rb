321d10b5914d7c35f08308d66658a530dfdacee2
alexanderernst <leverkusener77@googlemail.com>
2016-10-21 09:35:19 +0200

    added input fields closes #2

--- spec/features/authors_spec.rb
+++ spec/features/authors_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 describe "New Author Page", :type => :feature do
+  before(:each) do
+    visit new_author_path
+  end
   
   it "should render'" do
-    visit new_author_path
     expect(page).to have_text("New Author")
   end
 
   it "should have input for firstname, lastname, homepage" do
-    visit new_author_path
     expect(page).to have_field("first name")
     expect(page).to have_field("last name")
     expect(page).to have_field("homepage")
